1d660eb0bffd45432115fb20a7723ae82b790cfe
Henree <hha1@ualberta.ca>
2015-01-29 23:00:33 -0700

    Added another layout

--- gen/ca/ualberta/cs/example/traveltracker/R.java
+++ gen/ca/ualberta/cs/example/traveltracker/R.java
@@ -26,27 +26,32 @@ public final class R {
     }
     public static final class id {
         public static final int AddClaimsButton=0x7f080002;
-        public static final int ClaimsAddButton=0x7f080007;
-        public static final int CurrentClaimsTitle=0x7f080004;
-        public static final int DeleteExpenseButton=0x7f080005;
-        public static final int EditExpenseButton=0x7f080006;
-        public static final int ExpenseAdd=0x7f08000e;
-        public static final int ExpenseCat=0x7f08000a;
-        public static final int ExpenseCost=0x7f08000b;
-        public static final int ExpenseCurrency=0x7f08000c;
-        public static final int ExpenseDate=0x7f080008;
-        public static final int ExpenseDetail=0x7f08000d;
-        public static final int ExpenseName=0x7f080009;
-        public static final int ExpenseTitle=0x7f08000f;
+        public static final int ClaimsAddButton=0x7f08000b;
+        public static final int CurrentClaimsTitle=0x7f080008;
+        public static final int DeleteExpenseButton=0x7f080009;
+        public static final int EditExpenseButton=0x7f08000a;
+        public static final int ExpenseAdd=0x7f080012;
+        public static final int ExpenseCat=0x7f08000e;
+        public static final int ExpenseCost=0x7f08000f;
+        public static final int ExpenseCurrency=0x7f080010;
+        public static final int ExpenseDate=0x7f08000c;
+        public static final int ExpenseDetail=0x7f080011;
+        public static final int ExpenseName=0x7f08000d;
+        public static final int ExpenseTitle=0x7f080013;
         public static final int MainScreenTitle=0x7f080001;
         public static final int TravelClaims=0x7f080000;
-        public static final int action_settings=0x7f080010;
+        public static final int action_settings=0x7f080014;
+        public static final int addClaimFinish=0x7f080006;
+        public static final int addClaimName=0x7f080004;
+        public static final int addClaims=0x7f080007;
+        public static final int claimStartDate=0x7f080005;
         public static final int listView1=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int travelclaim=0x7f030001;
-        public static final int travelexpense=0x7f030002;
+        public static final int addclaim=0x7f030001;
+        public static final int travelclaim=0x7f030002;
+        public static final int travelexpense=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/ca/ualberta/cs/traveltracker/TravelExpenses.java
+++ src/ca/ualberta/cs/traveltracker/TravelExpenses.java
@@ -3,4 +3,13 @@ package ca.ualberta.cs.traveltracker;
 public class TravelExpenses 
 {
 
+	public TravelExpenses(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
